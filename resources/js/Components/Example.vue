26fa9ba5b4513b916a35466a977e0fc886ad9c35
Manmeet Singh <manmeetsingh@Manmeets-MacBook-Air.local>
2024-12-01 14:14:44 +0530

    first commit

---
+++ resources/js/Components/Example.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+<div class="flex items-center border border-slate-500 text-center w-1/2 justify-center pl-100 ">
+    <div class="text-2xl py-3 text-red-800" > Component Used from Components folder
+         Path: resources/js/Components/Example.vue</div>  
+</div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Example from '../Components/Example.vue';
+
+</script>
+
+<template>
+    <H1 class="text-3xl text-center py-5">Pre Insatlled Laravel Vue Js Inertia Js and Tailwind CSS <br/>Boilerplate Temaplete for starting new VLIT Stack
+         </H1>
+         <div class="flex items-center pl-20">
+         <Example />
+         </div>
+</template>
